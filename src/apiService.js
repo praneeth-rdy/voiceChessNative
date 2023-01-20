f246d4345e7026e204873718b05e52daebeb0708
praneeth-rdy <praneeth.kolanu.iitkgp@gmail.com>
2023-01-20 21:05:23 +0530

    Project setup

---
+++ src/apiService.js
@@ -0,0 +1,8 @@
+const apiService = 'http://10.145.205.74:5050';
+// const apiService = 'http://192.168.29.125';
+// const apiService = 'http://192.168.29.125:5050';
+// const apiService = 'https://gymkhana.iitkgp.ac.in';
+// Mobile Hotspot
+// const apiService = 'http://192.168.184.201:5050';
+
+export default apiService;
