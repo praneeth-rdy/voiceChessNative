94f6d2b6be1dc00afe6d7670df0f4de80505d835
praneeth-rdy <praneeth.kolanu.iitkgp@gmail.com>
2023-01-22 18:04:22 +0530

    Setup voice recognition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Temp from './screens/Temp';
 
 export default () => {
     // AsyncStorage.removeItem('authToken')
     return (
-        <View>
-            <Text>Welcome Screen</Text>
-        </View>
+        // <View>
+        //     <Text>Welcome Screen</Text>
+        // </View>
+        <Temp />
     );
 };
